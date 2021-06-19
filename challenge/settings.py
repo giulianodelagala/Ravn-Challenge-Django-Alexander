8d17bab89d688112129c1b4c555c1cd7375e34be
giulianodelagala <alexander.pinto@ucsp.edu.pe>
2021-06-19 14:07:51 -0500

    admin panel

--- challenge/settings.py
+++ challenge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'swapi.apps.SwapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
