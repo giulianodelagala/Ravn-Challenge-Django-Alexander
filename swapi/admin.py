1f1f5cb58d15cf99b891bbc93c3d7682061a9967
giulianodelagala <alexander.pinto@ucsp.edu.pe>
2021-06-19 15:33:40 -0500

    Basic models

--- swapi/admin.py
+++ swapi/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import People
+from .models import Planets
+from .models import Vehicles
+from .models import Species
+
+admin.site.register(People)
+admin.site.register(Planets)
+admin.site.register(Vehicles)
+admin.site.register(Species)
--- swapi/models.py
+++ swapi/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+class Planets(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Vehicles(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Species(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class People(models.Model):
+    name = models.CharField(max_length=60)
+    #heigth = models.IntegerField(min=0)
+    eye_color = models.CharField(max_length=20)
+    hair_color = models.CharField(max_length=20)
+    skin_color = models.CharField(max_length=20)
+    birth_year = models.CharField(max_length=12)
+    homeworld = models.ForeignKey(Planets, on_delete=models.CASCADE)
+    vehicles = models.ManyToManyField(Vehicles)
+    species = models.ManyToManyField(Species)
+
+    def __str__(self):
+        return self.name
+
+
+
+
